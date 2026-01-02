d38f5ecd0d06c10da6d818003525b84a93475ac4
jenil <jenilsakriya612@gmail.com>
2026-01-02 17:14:05 +0530

    separeted controllers from role routes

---
+++ backend/controllers/adminControllers/role.controller.js
@@ -0,0 +1,48 @@
+import prisma from "../../prisma/client.js";
+
+export async function addRole(req, res) {
+    const { rolename } = req.body;
+
+    try {
+        const isRole = await prisma.role.findUnique({
+            where: {
+                rolename: rolename,
+            }
+        })
+        if (isRole) {
+            return res.status(500).json({ message: "role already exists ..." });
+        }
+
+        await prisma.role.create({
+            data: {
+                rolename: rolename,
+            }
+        })
+
+        return res.status(200).json({ message: "Role added successfully ..." });
+    } catch (error) {
+        return res.status(400).json({ error: error.message });
+    }
+}
+
+export async function deleteRole(req, res) {
+    const { rolename } = req.body;
+
+    const isRole = await prisma.role.findUnique({
+        where: {
+            rolename: rolename,
+        }
+    })
+
+    if (!isRole) {
+        return res.status(404).json({ message: "could not found this role..." });
+    }
+
+    const deleteRole = await prisma.role.delete({
+        where: {
+            rolename: rolename,
+        }
+    })
+
+    return res.status(200).json({ message: "role deleted successfully ...", deleteRole });
+}
