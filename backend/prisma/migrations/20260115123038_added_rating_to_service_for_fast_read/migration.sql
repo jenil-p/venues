67e39da5ab9479171cf1a5cc67b47d3c77e80be8
jenil <jenilsakriya612@gmail.com>
2026-01-15 18:09:10 +0530

    added rating to services as well for fast read

---
+++ backend/prisma/migrations/20260115123038_added_rating_to_service_for_fast_read/migration.sql
@@ -0,0 +1,37 @@
+/*
+  Warnings:
+
+  - You are about to alter the column `totalCost` on the `Booking` table. The data in that column could be lost. The data in that column will be cast from `Decimal(65,30)` to `Decimal(10,2)`.
+  - You are about to alter the column `priceAtBooking` on the `BookingService` table. The data in that column could be lost. The data in that column will be cast from `Decimal(65,30)` to `Decimal(10,2)`.
+  - You are about to alter the column `amount` on the `Payment` table. The data in that column could be lost. The data in that column will be cast from `Decimal(65,30)` to `Decimal(10,2)`.
+  - You are about to alter the column `basePrice` on the `Service` table. The data in that column could be lost. The data in that column will be cast from `Decimal(65,30)` to `Decimal(10,2)`.
+  - You are about to alter the column `revenue` on the `ServiceStatistic` table. The data in that column could be lost. The data in that column will be cast from `Decimal(65,30)` to `Decimal(10,2)`.
+  - You are about to alter the column `rating` on the `Venue` table. The data in that column could be lost. The data in that column will be cast from `Decimal(65,30)` to `Decimal(3,2)`.
+  - You are about to alter the column `price` on the `VenuePricingRule` table. The data in that column could be lost. The data in that column will be cast from `Decimal(65,30)` to `Decimal(10,2)`.
+  - You are about to alter the column `revenue` on the `VenueStatistic` table. The data in that column could be lost. The data in that column will be cast from `Decimal(65,30)` to `Decimal(10,2)`.
+
+*/
+-- AlterTable
+ALTER TABLE "Booking" ALTER COLUMN "totalCost" SET DATA TYPE DECIMAL(10,2);
+
+-- AlterTable
+ALTER TABLE "BookingService" ALTER COLUMN "priceAtBooking" SET DATA TYPE DECIMAL(10,2);
+
+-- AlterTable
+ALTER TABLE "Payment" ALTER COLUMN "amount" SET DATA TYPE DECIMAL(10,2);
+
+-- AlterTable
+ALTER TABLE "Service" ADD COLUMN     "rating" DECIMAL(3,2) NOT NULL DEFAULT 0.0,
+ALTER COLUMN "basePrice" SET DATA TYPE DECIMAL(10,2);
+
+-- AlterTable
+ALTER TABLE "ServiceStatistic" ALTER COLUMN "revenue" SET DATA TYPE DECIMAL(10,2);
+
+-- AlterTable
+ALTER TABLE "Venue" ALTER COLUMN "rating" SET DATA TYPE DECIMAL(3,2);
+
+-- AlterTable
+ALTER TABLE "VenuePricingRule" ALTER COLUMN "price" SET DATA TYPE DECIMAL(10,2);
+
+-- AlterTable
+ALTER TABLE "VenueStatistic" ALTER COLUMN "revenue" SET DATA TYPE DECIMAL(10,2);
